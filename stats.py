2edcc7d266c907d282cc879297eb20415219c259
Rignchen <119439839+Rignchen@users.noreply.github.com>
2023-01-23 20:47:45 +0100

    stats.py

---
+++ stats.py
@@ -0,0 +1,37 @@
+from os import listdir, system
+
+bad = []
+system("cls")
+
+def surch(extension:list[str],path:str,function = None,exception = None):
+	out = 0
+	if path[-1] != "/": path += "/"
+	for a in listdir(path):
+		if exception == None or path + a not in exception:
+			try:
+				listdir(path + a)
+				out += surch(extension,path + a,function)
+			except NotADirectoryError:
+				b = a.split(".")
+				if b[-1] in extension:
+					out += 1
+					if function:function(path + a)
+	return out
+
+def func(path):
+	global bad
+	try:
+		a = open(path)
+		a.readlines()
+		a.close()
+		bad.append(path)
+	except UnicodeDecodeError:
+		pass
+
+print("advancements:" + str(surch(["json"],"data/galactipack/advancements")) + " (display: " + str(surch(["json"],"data/galactipack/advancements/display")) + ")")
+print("functions:" + str(surch(["mcfunction"],"data/galactipack/functions",func)))
+print("predicates:" + str(surch(["json"],"data/galactipack/predicates")))
+print("recipes:" + str(surch(["json"],"data/galactipack/recipes")))
+print("items:" + str(surch(["json"],"data/galactipack/loot_tables/items")))
+print("loot tables:" + str(surch(["json"],"data/galactipack/loot_tables")))
+if bad != []:print("/!\\ " + str(len(bad)) + " fonctions aren't comment: " + str(bad))
